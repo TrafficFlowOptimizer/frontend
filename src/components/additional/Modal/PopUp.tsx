6de3058fb24f6a1d2653d40d0b09a89014b89ece
Nikodem Korohoda <nikodemko@interia.pl>
2023-05-30 02:10:30 +0200

    memes in loading screen 8)

--- src/components/additional/Modal/PopUp.tsx
+++ src/components/additional/Modal/PopUp.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ThemeContext } from "../../../custom/ThemeContext";
@@ -10,21 +10,43 @@ export type PopUpProps = {
 	// onClose(): void;
 };
 
+import meme_0 from "../../../assets/memes/changing_code.jpg";
+import meme_1 from "../../../assets/memes/meme_1.jpg";
+import meme_2 from "../../../assets/memes/meme_2.png";
+import meme_3 from "../../../assets/memes/meme_3.jpg";
+
+let memes: any[] = [meme_0, meme_1, meme_2, meme_3];
+
 export function PopUp(props: PopUpProps) {
-	const { theme } = useContext(ThemeContext);
-	return (
-		<StyledModal>
-			<FontAwesomeIcon
-				icon={faCloud}
-				beatFade
-				size="xl"
-				style={{
-					color: theme === "dark" ? LightTheme.primary : DarkTheme.primary,
-				}}
-			/>
-			<StyledMessageField>{props.textToDisplay}</StyledMessageField>
-		</StyledModal>
-	);
+  const [image, setImage] = useState(meme_0);
+  const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setImage((prevImage) => {
+        for(let i=0;i<memes.length-1;i++){
+          if(prevImage===memes[i]) return memes[i+1];
+        }
+        return memes[0];
+      });
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <StyledModal>
+      <FontAwesomeIcon
+        icon={faCloud}
+        beatFade
+        size="xl"
+        style={{
+          color: theme === "dark" ? LightTheme.primary : DarkTheme.primary
+        }}
+      />
+      <StyledMessageField>{props.textToDisplay}</StyledMessageField>
+      <img src={image} alt="Image" style={{ width: "500px", height: "500px" }} />
+    </StyledModal>
+  );
 }
 
 export const StyledModal = styled.div`
